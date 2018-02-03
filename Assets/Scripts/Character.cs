d7c502f6d434577dc532aeba1c64293cb58c98c5
marlord <marlon_dx@hotmail.com>
2018-02-03 16:22:24 -0600

    Version Final
    Previamente se realizo un commit donde el codigo estaba incorrecto. Se quito el commit y se arreglo los errores. En este commit esta el laboratorio terminado.

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class Character : MonoBehaviour {
 
+    Animator anim;
     Rigidbody2D rb2d;
     SpriteRenderer sr;
     public Camera cam;
@@ -14,6 +15,7 @@ public class Character : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        anim = GetComponent<Animator>();
         rb2d = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
         cam.transform.position = new Vector3(rb2d.transform.position.x, cam.transform.position.y, cam.transform.position.z);
@@ -22,6 +24,7 @@ public class Character : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         float move = Input.GetAxis("Horizontal");
+        anim.SetFloat("Speed", move);
         if (move != 0) {
             rb2d.transform.Translate(new Vector3(1, 0, 0) * move * speed * Time.deltaTime);
             cam.transform.position = new Vector3(rb2d.transform.position.x, cam.transform.position.y, cam.transform.position.z);
